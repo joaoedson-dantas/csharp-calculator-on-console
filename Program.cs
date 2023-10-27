146a371f7829302bb28d5ae99c6241cb0feac6f9
João Edson <joaoedson.dantas@outlook.com>
2023-10-27 18:39:01 -0300

    feat: adicionado um função de menu a calculadora

--- Program.cs
+++ Program.cs
@@ -5,9 +5,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Multiplicacao();
+        Menu();
     }
 
+    static void Menu()
+    {
+        Console.Clear();
+
+        Console.WriteLine("Qual operação você deseja fazer?");
+        Console.WriteLine("1 - Soma");
+        Console.WriteLine("2 - Subtração");
+        Console.WriteLine("3 - Divisão");
+        Console.WriteLine("4 - Multiplicação");
+
+        Console.WriteLine("----------");
+
+        Console.WriteLine("Selecione uma opção: ");
+        short res = short.Parse(Console.ReadLine());
+
+        switch (res)
+        {
+            case 1: Soma(); break;
+            case 2: Subtracao(); break;
+            case 3: Divisao(); break;
+            case 4: Multiplicacao(); break;
+            default: Menu(); break;
+        }
+
+
+    }
     static void Soma()
     {
         Console.Clear();
@@ -23,6 +49,7 @@ class Program
         Console.WriteLine($"O resultado da soma é: {resultado}");
 
         Console.ReadKey();
+        Menu();
     }
 
     static void Subtracao()
@@ -39,6 +66,8 @@ class Program
         float resultado = v1 - v2;
         Console.WriteLine($"O resultado da subtração é {resultado}");
         Console.ReadKey();
+
+        Menu();
 
 
     }
@@ -58,6 +87,8 @@ class Program
 
         Console.WriteLine($"O resultado da divisão é {resultado}");
         Console.ReadKey();
+
+        Menu();
     }
 
     static void Multiplicacao()
@@ -74,5 +105,7 @@ class Program
 
         Console.WriteLine($"O resultado da multiplicação é {resultado}");
         Console.ReadKey();
+
+        Menu();
     }
 }
