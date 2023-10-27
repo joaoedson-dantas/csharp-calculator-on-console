fa8d9f792ed4161c98deba4ea6c8c67f5fbeb565
João Edson <joaoedson.dantas@outlook.com>
2023-10-27 17:48:36 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Calculator;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Soma();
+    }
+
+    static void Soma()
+    {
+        Console.Clear();
+
+        Console.WriteLine("Primeiro valor: ");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite um segundo valor: ");
+        float v2 = float.Parse(Console.ReadLine());
+
+        float resultado = v1 + v2;
+        Console.WriteLine("");
+        Console.WriteLine($"O resultado da soma é: {resultado}");
+    }
+}
