ac1a520dc20436e6796bcd790665b9f750d381c5
João Edson <joaoedson.dantas@outlook.com>
2023-10-27 17:57:59 -0300

    adicionado o método de subtração ao calculadora

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Soma();
+        Subtracao();
     }
 
     static void Soma()
@@ -19,5 +19,25 @@ class Program
         float resultado = v1 + v2;
         Console.WriteLine("");
         Console.WriteLine($"O resultado da soma é: {resultado}");
+
+        Console.ReadKey();
+    }
+
+    static void Subtracao()
+    {
+        Console.Clear();
+        Console.WriteLine("Primeiro valor:");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Segundo valor:");
+        float v2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("");
+
+        float resultado = v1 - v2;
+        Console.WriteLine($"O resultado da subtração é {resultado}");
+        Console.ReadKey();
+
+
     }
 }
